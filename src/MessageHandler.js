27dbff181b30e7c5f2f00335bdc3a04e7e5f77ff
asukii314 <asukii314.twitch@gmail.com>
2021-04-16 21:47:17 -0400

    Show extra metadata in tooltip

--- src/MessageHandler.js
+++ src/MessageHandler.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import WheelComponent from 'react-wheel-of-prizes'
+import ReactTooltip from 'react-tooltip'
+import './messageStyles.css';
 const randomColor = require('randomcolor');
 const tmi = require('tmi.js');
 
@@ -15,18 +17,35 @@ const filterGameCommands = function(message) {
 
 
 class GameRequest extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timeDiff: 0
+    };
+  }
 
+  updateTimeDiff = () => {
+    // return Date.now();
+    this.setState(() => {
+      return {
+        timeDiff: `${Math.floor((Date.now()-this.props.metadata.time)/60000)} minutes ago`
+      };
+    });
+  }
   delete = (e) => {
     this.props.onDelete(this.props.msg)
   }
 
   render() {
     return (
-      <div style={{backgroundColor:'steelblue', textAlign: 'left', borderRadius: '8px', textTransform: 'capitalize'}}>
-      <p style={{margin: '15px', padding: '4px', fontSize: '24px', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+      <div>
+      <ReactTooltip effect="solid" place="right"/>
+      <div id="baseDiv" data-tip={`Requested ${this.state.timeDiff} by ${this.props.metadata.username}`} style={{backgroundColor:'steelblue', textAlign: 'left', borderRadius: '8px', textTransform: 'capitalize'}} onMouseEnter={this.updateTimeDiff}>
+      <p style={{margin: '15px', padding: '4px', fontSize: '24px', display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
+        <button type='button' className="deleteButton" onClick={this.delete}>X</button>
         {this.props.msg}
-        <button type='button' onClick={this.delete} style={{backgroundColor:'red', float: 'right', height: '50%', color: 'white', fontSize: '16px'}}>X</button>
       </p>
+      </div>
       </div>
     );
   }
@@ -69,7 +88,7 @@ export default class MessageHandler extends Component {
     this.setState((state) => {
       return {
         ...state,
-        messages: {...this.state.messages, [game]: {user: tags.username, time: Date.now()}},
+        messages: {...this.state.messages, [game]: {username: tags.username, time: Date.now()}},
         counter: this.state.counter + 1
       };
     })
@@ -112,7 +131,7 @@ export default class MessageHandler extends Component {
         <column width="50vw">
           <h2 style={{marginBottom:"0"}}>Game Requests</h2>
           <h4 style={{fontSize:"12px", marginTop: "6px", marginBottom:"12px", fontWeight: 400}}>Type e.g. "!request Blather Round" in {this.props.channel}'s chat to add</h4>
-          {gameArray.map((msg, i) => <GameRequest key={i} msg={msg} onDelete={this.removeGame}/>)}
+          {gameArray.map((msg, i) => <GameRequest key={i} msg={msg} metadata={this.state.messages[msg]} onDelete={this.removeGame}/>)}
         </column>
         <column width="50vw" style={{textTransform: 'capitalize'}}>
           <div style={{fontSize: "16px", overflow: "hidden", width: "600px"}}>
