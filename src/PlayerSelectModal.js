74f721aaa24404f605011c5d43f9e9792f63f339
asukii314 <asukii314.twitch@gmail.com>
2021-05-29 01:37:12 -0400

    fail gracefully if wheel is opened with no game in queue

--- src/PlayerSelectModal.js
+++ src/PlayerSelectModal.js
@@ -224,7 +224,7 @@ export default class PlayerSelectModal extends Component {
         <div className="header">
           {this.renderStreamerSeatToggle()}
           <div style={{marginTop: '10px'}}>
-          <b>{this.props.game.name}</b>
+          <b>{this.props.game?.name ?? "TBD"}</b>
           {this.renderPlayerCount()}
           </div>
           <button className={startGameClass} onClick={this.startGame} disabled={!this.canStartGame()}>Start Game</button>
