12d508d798ec5c65e257df3daa8410e62d7b4fd1
dcpesses <dcpesses@users.noreply.github.com>
2021-07-30 14:48:29 -0700

    refactor(package): updated scripts to perform unit tests before builds and deployments

--- tests/test.sh
+++ tests/test.sh
@@ -4,14 +4,36 @@ echo "Cleaning coverage files"
 rm -rf coverage
 
 echo ""
-echo "Checking files with ESLint"
+echo "Checking scripts with ESLint"
 ./node_modules/.bin/eslint src/
 if [ "$?" -ne 0 ]
 then
+    echo ""
+    echo ""
+    echo "*** LINTING ERROR DISCOVERED; ABORTING ***"
+    echo ""
+    echo "Please make sure your scripts do not have any linting errors"
+    echo ""
+    echo ""
     exit 1
 fi
-echo " done"
+echo ""
+echo "√ ESLint check completed, okay to continue"
 
 echo ""
-echo "Running unit tests with Jest"
+echo "Running test suites with Jest"
 npm run test -- --ci --coverage --watchAll=false
+if [ "$?" -ne 0 ]
+then
+    echo ""
+    echo ""
+    echo "*** TESTING ERROR DISCOVERED; ABORTING ***"
+    echo ""
+    echo "Please make sure your unit-tests do not have any linting issues"
+    echo "and that none of your snapshots are listed as obsolete"
+    echo ""
+    echo ""
+    exit 1
+fi
+echo ""
+echo "√ Test suites completed successfully"
