6a9d3a4ccdb039b6c634dcea3603f6c02374f104
dcpesses <dcpesses@users.noreply.github.com>
2023-12-13 12:10:41 -0800

    feat(app): added theme support based on settings or hash variable

--- src/App.js
+++ src/App.js
@@ -9,11 +9,41 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            theme: localStorage.getItem('__theme'),
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('hashchange', this.onHashChange);
+        this.onHashChange();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('hashchange', this.onHashChange);
+    }
+
+    onHashChange = () => {
+        // check for theme changes
+        let themeMatch = window.location.hash.match(/theme=(.+?)\b/);
+        if (themeMatch !== null && themeMatch[1] !== this.state.theme) {
+            this.setState({
+                theme: themeMatch[1]
+            });
+        }
+    }
+
     render() {
 
         let classNames = ['App'];
         if (window.location.hash.indexOf('legacy=true') !== -1) {
             classNames.push('legacy');
+        }
+
+        if (this.state.theme) {
+            classNames.push(`theme theme-${this.state.theme}`);
         }
 
         return (
